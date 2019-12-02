eb67a37da3571d412277df00de9b7a3192f2c3e7
sch <1208579668@qq.com>
2019-12-02 13:57:20 +0800

    rabbitmq：topic交换器（环境搭建）

---
+++ dubbo-rabbitmq/topic-provider/src/test/java/com/sch/topicprovider/TopicProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sch.topicprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
